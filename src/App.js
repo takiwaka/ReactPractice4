d1b771de4ca26269f089fa7cc0cc33df3062fa11
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-12-01 05:37:48 +0900

    router切り出しの基本

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
@@ -18,34 +14,7 @@ export default function App() {
         <Link to="/Page2">Page2</Link>
         <br />
       </div>
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/Page1"
-          render={({ match: { url } }) => (
-            <Switch>
-              {console.log(url)}
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/DetailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/DetailB`}>
-                <Page1DetailB />
-              </Route>
-              <Page1 />
-            </Switch>
-          )}
-        />
-
-        <Route path="/Page2">
-          <Page2 />
-        </Route>
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/Page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+
+      <Route path="/Page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
