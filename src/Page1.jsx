f855610d676167c412bbf3321cfe8a47e30c6874
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-12-01 06:55:16 +0900

    ページ遷移時に情報を渡す

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <div>
       <h1>ページ1</h1>
-      <Link to="/Page1/detailA">detailA</Link>
+      <Link to={{ pathname: "/Page1/detailA", state: arr }}>detailA</Link>
       <br />
       <Link to="/Page1/detailB">detailB</Link>
     </div>
