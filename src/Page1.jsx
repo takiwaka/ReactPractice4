ec13408f8e97a9a4c32f14d2532b6e0edc4b3c40
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-12-02 03:54:55 +0900

    Javascript側での画面遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   const arr = [...Array(100).keys()];
+
+  const history = useHistory();
+
+  const onClickDetailA = () => history.push("/page1/detailA");
+
   return (
     <div>
       <h1>ページ1</h1>
       <Link to={{ pathname: "/Page1/detailA", state: arr }}>detailA</Link>
       <br />
       <Link to="/Page1/detailB">detailB</Link>
+      <br />
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
